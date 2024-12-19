46c86446a7a57fbaa850d0801a5d9a05328b7c05
ceptiloid <nikolaidichenko@gmail.com>
2024-12-20 02:02:35 +0300

    пример 2

---
+++ Пример 2.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    word = input("Введите слово: ")
+    idx = len(word) // 2
+    if len(word) % 2 == 1:
+        r = word[:idx] + word[idx+1:]
+    else:
+        r = word[:idx-1] + word[idx+1:]
+    print(r)
